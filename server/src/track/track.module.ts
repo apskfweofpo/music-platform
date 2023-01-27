0e123bf9a87406be0044962f2b023f65cf4b3cf2
Pavel Krikunov <fusrodah.df@mail.ru>
2023-01-27 23:59:05 +0300

    add module for upload files(picture and image)

--- server/src/track/track.module.ts
+++ server/src/track/track.module.ts
@@ -4,6 +4,8 @@ import {TrackService} from "./track.service";
 import {MongooseModule} from "@nestjs/mongoose";
 import {Track, TrackSchema} from "./schemes/track.schema";
 import {Comment, CommentSchema} from "./schemes/comment.schema";
+import {FileModule} from "../file/file.module";
+import {FileService} from "../file/file.service";
 
 
 @Module({
@@ -12,7 +14,7 @@ import {Comment, CommentSchema} from "./schemes/comment.schema";
         MongooseModule.forFeature([{name: Comment.name, schema: CommentSchema}]),
     ],
     controllers:[TrackController],
-    providers:[TrackService]
+    providers:[TrackService, FileService]
 
 })
 export class TrackModule {}
